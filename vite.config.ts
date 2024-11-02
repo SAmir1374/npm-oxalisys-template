22917d83e421275f4d42eeb9dbab34aaa5e1b3cd
Amirmuhammad <s.amir.79926@gmail.com>
2024-11-02 21:20:41 +0330

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "OxalisysTemplate",
+      fileName: (format) => `oxalisys-template.${format}.js`,
+    },
+    rollupOptions: {
+      // Exclude dependencies from the output bundle
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
